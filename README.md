d50be3263731d931292acdefb99a7a2422d96cef
KetchupKingJ <108550500+KetchupKing1@users.noreply.github.com>
2023-07-06 23:43:57 +0100

    Code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Web 3 ReactApp
   A Web 3.0 application that used React.JS as the front end
+run nmp install to install all the dependacies
+then run nmp run start to start it
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import "../App.css";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { ethers, BigNumber } from "ethers";
+import Results from "../Results.json";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import ReactModal from "react-modal";
+import Header from "./Header";
+
+const Home = () => {
+  return <div>
+    <h1>Home</h1>
+    
+    </div>;
+};
+
+export default Home;
+
